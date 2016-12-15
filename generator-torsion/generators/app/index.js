e0f76b1fb016804557b636e94e0504f63131c5c0
Micah Joyner <micah@cheekymonkeymedia.ca>
2016-12-15 14:12:43 -0800

    bower and package JSON files will be built with yeoman

--- generator-torsion/generators/app/index.js
+++ generator-torsion/generators/app/index.js
@@ -3,9 +3,8 @@ var generators = require('yeoman-generator');
 var yosay = require('yosay');
 
 module.exports = generators.Base.extend({
-  // The name `constructor` is important here
+
   constructor: function () {
-    // Calling the super constructor is important so our generator is correctly set up
     generators.Base.apply(this, arguments);
   },
 
@@ -15,5 +14,36 @@ module.exports = generators.Base.extend({
 
   prompting: function () {
     this.log(yosay('Torsion by Cheeky Monkey Media'));
+
+    var prompts = [{
+      type: 'checkbox',
+      name: 'frameworks',
+      message: 'Which framework would you like to include?',
+      choices: [{
+        name: 'Foundation',
+        value: 'includeFoundation',
+        checked: true
+      }, {
+        name: 'Bootstrap',
+        value: 'includeBootstrap',
+        checked: true
+      }
+      }]
+    }];
+
+    return this.prompt(prompts).then(function (answers) {
+      var features = answers.features;
+
+      function hasFeature(feat) {
+        return features && features.indexOf(feat) !== -1;
+      };
+
+      // manually deal with the response, get back and store the results.
+      // we change a bit this way of doing to automatically do this in the self.prompt() method.
+      this.includeFoundation = hasFeature('includeFoundation');
+      this.includeBootstrap = hasFeature('includeBootstrap');
+    }.bind(this));
+
+
   }
 });
