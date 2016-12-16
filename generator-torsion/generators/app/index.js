3bfabe94044954c122267d5afffc4ddca8c55b4a
Micah Joyner <micah@cheekymonkeymedia.ca>
2016-12-16 14:01:09 -0800

    more yeoman work

--- generator-torsion/generators/app/index.js
+++ generator-torsion/generators/app/index.js
@@ -1,6 +1,8 @@
 'use strict';
+
 var generators = require('yeoman-generator');
 var yosay = require('yosay');
+var _s = require('underscore.string');
 
 module.exports = generators.Base.extend({
 
@@ -13,53 +15,96 @@ module.exports = generators.Base.extend({
   },  
 
   prompting: function () {
-    this.log(yosay('Torsion by Cheeky Monkey Media'));
+    this.log(yosay('Torsion - Monkeys are sexy.'));
 
     var prompts = [{
-      type: 'checkbox',
-      name: 'frameworks',
-      message: 'Which framework would you like to include?',
+      type: 'list',
+      name: 'cms',
+      message: 'Which CMS are you building for?',
       choices: [{
-        name: 'Foundation',
-        value: 'includeFoundation',
+        name: 'None (Static Website)',
+        value: 'none',
         checked: false
-      //}, {
-        //name: 'Bootstrap',
-        //value: 'includeBootstrap',
-        //checked: true
+      }, {
+        name: 'Drupal 8',
+        value: 'includeD8',
+        checked: false
+      }, {
+        name: 'Drupal 7',
+        value: 'includeD7',
+        checked: true
+      }, {
+        name: 'Wordpress',
+        value: 'includeWP',
+        checked: false
+      }]
+    },{
+      type: 'checkbox',
+      name: 'features',
+      message: 'Additional Features (check all that apply): ',
+      choices: [{
+        name: 'Modernizr',
+        value: 'includeModernizr',
+        checked: 'false'
+      },{
+        name: 'jQuery',
+        value: 'includejQuery',
+        checked: 'false'
       }]
     }];
 
     return this.prompt(prompts).then(function (answers) {
-      var frameworks = answers.frameworks;
+      var cms = answers.cms;
 
-      function hasFeature(framework) {
-        return frameworks && frameworks.indexOf(framework) !== -1;
+      function hasFeature(cms) {
+        return cms && cms.indexOf(cms) !== -1;
       };
 
-      // manually deal with the response, get back and store the results.
-      // we change a bit this way of doing to automatically do this in the self.prompt() method.
-      this.includeFoundation = hasFeature('includeFoundation');
-      //this.includeBootstrap = hasFeature('includeBootstrap');
+      this.includeD8 = hasFeature('includeD8');
+      this.includeD7 = hasFeature('includeD7');
+      this.includeWP = hasFeature('includeWP');
+      this.includeModernizr = hasFeature('includeModernizr');
+      this.includejQuery = hasFeature('includejQuery');
+
     }.bind(this));
   },
 
   writing: {
+    configFiles: function () {
+      this.fs.copyTpl(
+        this.templatePath('_bowerrc'),
+        this.destinationPath('patternlab/.bowerrc')
+      ),
+      this.fs.copyTpl(
+        this.templatePath('_editorconfig'),
+        this.destinationPath('patternlab/.editorconfig')
+      ),
+      this.fs.copyTpl(
+        this.templatePath('_jshintrc'),
+        this.destinationPath('patternlab/.jshintrc')
+      );
+    },
     packageJSON: function () {
       this.fs.copyTpl(
         this.templatePath('_package.json'),
-        this.destinationPath('UI/package.json')
+        this.destinationPath('patternlab/package.json')
       );
     },    
     bowerJSON: function () {
-      this.fs.copyTpl(
-        this.templatePath('_bower.json'),
-        this.destinationPath('UI/bower.json'),
-        {
-          includeFoundation: this.includeFoundation
-          //includeBootstrap: this.includeBootstrap
-        }
-      );
+      var bowerJson = {
+        name: _s.slugify(this.appname),
+        private: true,
+        dependencies: {}
+      };
+
+      if (this.includejQuery) {
+        bowerJson.dependencies['jquery'] = '~2.1.1';
+      }
+
+      if (this.includeModernizr) {
+        bowerJson.dependencies['modernizr'] = '~2.8.1';
+      }
+      this.fs.writeJSON('patternlab/bower.json', bowerJson);
     }
   }
 });
